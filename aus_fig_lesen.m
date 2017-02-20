876a8fe71fb8abeb237785b3b24ade14094d70bd
Falko8 <falko.salzmann@web.de>
2017-02-20 19:17:00 +0100

    Delete aus_fig_lesen.m

--- aus_fig_lesen.m
+++
@@ -1,8 +0,0 @@
-cd('C:\Users\Pandorra\Desktop\Matlab_Projekte\01_Fahrzeugmodelle\Einspurmodell')
-f = openfig('Simulationsergebnis.fig');
-H=findobj(f,'type','line');
-x_data=get(H,'xdata');
-y_data=get(H,'ydata');
-%close all
-plot(x_data,y_data)
-size(y_data)
